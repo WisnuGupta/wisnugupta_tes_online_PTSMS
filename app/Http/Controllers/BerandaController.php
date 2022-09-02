3f1044ed3e1a98750c8622301b20754fecb70770
Wisnu Gupta <wisnu21gupta@gmail.com>
2022-09-02 21:07:33 +0700

    menambahkan card total data barang

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Barang;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BerandaController extends Controller
 {
     public function index(){
-    return view('beranda');
+    $data=DB::table('tbl_barang')->paginate();
+    return view('beranda',compact('data'));
     }
 }
